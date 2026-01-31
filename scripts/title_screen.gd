5f3c2f6a72fbc4155934478b2f5ed2050a7e4462
Cole Cecil <cole.cecil@gmail.com>
2026-01-31 15:29:44 -0600

    Use actual game scene

--- scripts/title_screen.gd
+++ scripts/title_screen.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 func _on_start_button_pressed() -> void:
-	GameManager.change_scene('res://scenes/main.tscn')
+	GameManager.change_scene('res://scenes/Stages/Stage1.tscn')
 
 
 func _on_quit_button_pressed() -> void:
