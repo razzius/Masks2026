f90828db81aa7ae5f25403bf984693adec4ca316
Lynne Gonzalez <evlandesg@gmail.com>
2026-01-31 12:27:41 -0600

    Added gate logic

--- scripts/Gate.gd
+++ scripts/Gate.gd
@@ -1,16 +1,34 @@
 extends Node
 
 @export var opened : bool
+var sprite : Sprite2D
+var collider : Area2D
+
 
 func _ready() -> void:
+	sprite = get_child(0)
+	collider = get_child(1)
+	
 	if(opened):
-		pass
+		OpenDoor()
 	else:
-		pass
+		CloseDoor()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
+func OpenDoor() -> void:
+	sprite.visible = false
+	collider.disabled = true
+	opened = true
+	
+func CloseDoor() -> void:
+	sprite.visible = true
+	collider.disabled = false
+	opened = false
+	
 func ToggleDoor() -> void:
-	opened = !opened
+	if(opened):
+		CloseDoor()
+	else:
+		OpenDoor()
