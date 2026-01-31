aae6348d435a91e60f4baed0b990089d0494bc4c
Razzi Abuissa <razzi@abuissa.net>
2026-01-31 13:13:58 -0600

    Shift+escape to quit

---
+++ Managers/DebugManager.gd
@@ -0,0 +1,12 @@
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	if Input.is_action_pressed("shift+escape"):
+		get_tree().quit()
