2ff7382da7de25ffdf3b3d225b32379b3da43f4e
Razzi Abuissa <razzi@abuissa.net>
2026-01-30 21:46:20 -0600

    Janky left right player with gravity commented out

---
+++ maskcharacter.gd
@@ -0,0 +1,29 @@
+extends Sprite2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta) -> void:
+	if Input.is_action_pressed("Right"):
+		print("HIIIIIII")
+
+func _physics_process(delta: float) -> void:
+	var direction = Vector3.ZERO
+
+	# We check for each move input and update the direction accordingly.
+	if Input.is_action_pressed("Right"):
+		direction.x += 1
+	if Input.is_action_pressed("move_left"):
+		direction.x -= 1
+	if Input.is_action_pressed("move_back"):
+		# Notice how we are working with the vector's x and z axes.
+		# In 3D, the XZ plane is the ground plane.
+		direction.z += 1
+	if Input.is_action_pressed("move_forward"):
+		direction.z -= 1
+
+	move_and_slide()
