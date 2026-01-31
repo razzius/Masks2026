7d2c4f96022630120f9673ccd1abf0f817b82359
Cole Cecil <cole.cecil@gmail.com>
2026-01-31 11:48:39 -0600

    Add title screen

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,4 +1,18 @@
 extends Node
 
+var current_scene = null
+
 func _ready() -> void:
-	pass
+	var root = get_tree().root
+	current_scene = root.get_child(-1)
+
+# Change to the scene at the given path.
+func change_scene(path) -> void:
+	_deferred_change_scene.call_deferred(path)
+
+func _deferred_change_scene(path):
+	current_scene.free()
+	var s = ResourceLoader.load(path)
+	current_scene = s.instantiate()
+	get_tree().root.add_child(current_scene)
+	get_tree().current_scene = current_scene
---
+++ scripts/title_screen.gd
@@ -0,0 +1,4 @@
+extends Node2D
+
+func _on_start_button_pressed() -> void:
+	GameManager.change_scene('res://main.tscn')
