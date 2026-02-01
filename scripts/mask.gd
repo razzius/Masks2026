cf095b72611cc9a06758759ac9a6b4f6085b6d4e
Archives0 <evlandesg@gmail.com>
2026-02-01 04:17:04 -0600

    Added random variation to mask bounce

--- scripts/mask.gd
+++ scripts/mask.gd
@@ -3,8 +3,13 @@ class_name Mask
 signal OnMaskMerged
 
 @export var is_light : bool
+@export var minVelocity : float = 180
+@export var maxVelocity : float = 360
+@export var speedMod : float = 60
+
 var deleted = false
 var animSprite : AnimatedSprite2D
+var rng : RandomNumberGenerator = RandomNumberGenerator.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -18,10 +23,28 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 	
+	
+func Bounce() -> void:
+	var rb : RigidBody2D = get_child(0)
+	var currentV : Vector2 = rb.linear_velocity
+	var xMod = rng.randf_range(-speedMod, speedMod)
+	var yMod = rng.randf_range(-speedMod, speedMod)
+	
+	rb.linear_velocity.x += xMod
+	rb.linear_velocity.y += yMod
+	
+	if rb.linear_velocity.x < 0:
+		rb.linear_velocity.x = clampf(rb.linear_velocity.x, -minVelocity, -maxVelocity)
+	if rb.linear_velocity.x > 0:
+		rb.linear_velocity.x = clampf(rb.linear_velocity.x, minVelocity, maxVelocity)
+	if rb.linear_velocity.y < 0:
+		rb.linear_velocity.y = clampf(rb.linear_velocity.y, -minVelocity, -maxVelocity)
+	if rb.linear_velocity.y > 0:
+		rb.linear_velocity.y = clampf(rb.linear_velocity.y, minVelocity, maxVelocity)
+		
+	print(rb.linear_velocity)
 
 func DeleteSprite() -> void:
-	#deleted = true
-	#body.queue_free()
 	queue_free()
 	OnMaskMerged.emit()
 
@@ -37,5 +60,4 @@ func _on_rigid_body_2d_body_entered(body: Node) -> void:
 				animSprite.play("merge")
 			deleted = true
 			body.queue_free()
-			#queue_free()
-			#OnMaskMerged.emit()
+	Bounce()
