6c76b1407b7fd2edde31669393878f2f2046ceef
Archives0 <evlandesg@gmail.com>
2026-02-01 13:13:23 -0600

    Added impact sound effect

--- scripts/mask.gd
+++ scripts/mask.gd
@@ -11,11 +11,14 @@ signal OnMaskMerged
 var deleted = false
 var animSprite : AnimatedSprite2D
 var rng : RandomNumberGenerator = RandomNumberGenerator.new()
+var audioPlayer : AudioStreamPlayer2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	if is_light:
 		$RigidBody2D.set_collision_mask_value(2, true)
+	audioPlayer = $AudioStreamPlayer2D
+	#audioPlayer.volume_db
 	animSprite = $AnimatedSprite2D
 	animSprite.visible = false
 	animSprite.animation_finished.connect(DeleteSprite)
@@ -63,6 +66,7 @@ func _on_rigid_body_2d_body_entered(body: Node) -> void:
 				animSprite.play("merge")
 			deleted = true
 			body.queue_free()
+	audioPlayer.play()
 	Bounce()
 	if body.get_parent() as Spotlight:
 		if not is_light:
