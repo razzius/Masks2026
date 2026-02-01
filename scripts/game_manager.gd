1b4a4703cfa0c2a2bfae7de713e4a93f3a710a60
Cole Cecil <cole.cecil@gmail.com>
2026-01-31 20:11:43 -0600

    Escape key to exit to main menu

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -6,6 +6,10 @@ func _ready() -> void:
 	var root = get_tree().root
 	current_scene = root.get_child(-1)
 
+func _process(_delta: float) -> void:
+	if Input.is_action_pressed("Escape"):
+		exit_to_menu()
+
 # Change to the scene at the given path.
 func change_scene(path) -> void:
 	_deferred_change_scene.call_deferred(path)
@@ -16,6 +20,10 @@ func _deferred_change_scene(path):
 	current_scene = s.instantiate()
 	get_tree().root.add_child(current_scene)
 	get_tree().current_scene = current_scene
+
+func exit_to_menu() -> void:
+	if current_scene.name != 'TitleScreen':
+		change_scene('res://scenes/title_screen.tscn')
 
 # Exit the game
 func exit_game() -> void:
